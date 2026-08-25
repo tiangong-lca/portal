bba412b64865911b8845f9a6a696dbbd29a6f377
nanli <linanenv@gmail.com>
2026-08-26 02:11:38 +0800

    chore: ignore generated Next types

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
-  experimental: {
-    useTypeScriptCli: true,
-  },
 };
 
 export default nextConfig;
